94ddbdbb4051f78b2a45cef7fafd9ee96b280e3b
Alencar Morete <morete.alencar@gmail.com>
2025-04-23 20:23:28 -0400

    inicio do projeto

---
+++ Conta.cs
@@ -0,0 +1,33 @@
+﻿public class ContaBancaria
+{
+    public int IdConta;
+    public int Agencia;
+    public int NuConta;
+    public string TitularConta;
+    public double Saldo;
+
+    public void Depositar(double valorDep)
+    {
+        if (valorDep > 0)
+        {
+            Saldo += valorDep;
+        }
+        else
+        {
+            Console.WriteLine("Valor inválido!");
+        }
+    }
+
+    public void Sacar(double valorSaque)
+    {
+        if (valorSaque > 0 && valorSaque <= Saldo)
+        {
+            Saldo -= valorSaque;
+        }
+        else
+        {
+            Console.WriteLine("Valor inválido ou saldo insuficiente.");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,220 @@
+﻿List<ContaBancaria> contas = new List<ContaBancaria>();
+
+bool encerrarPrograma = false;
+while (!encerrarPrograma)
+{
+    Console.Clear();
+    Console.WriteLine("==============================");
+    Console.WriteLine("     CADASTRO DE CONTAS");
+    Console.WriteLine("==============================");
+    Console.WriteLine("1 - Cadastrar Conta");
+    Console.WriteLine("2 - Listar Contas");
+    Console.WriteLine("3 - Depositar");
+    Console.WriteLine("4 - Sacar");
+    Console.WriteLine("5 - Filtrar por saldo");
+    Console.WriteLine("6 - Saldo Total das Contas");
+    Console.WriteLine("0 - Sair");
+    Console.WriteLine("==============================");
+    Console.Write("Escolha uma opção: ");
+    int opcao = Convert.ToInt32(Console.ReadLine());
+
+    switch (opcao)
+    {
+        case 0:
+            {
+                Console.WriteLine("Encerrando o programa...");
+                encerrarPrograma = true;
+                break;
+            }
+        case 1:
+            CadastrarConta();
+            break;
+        case 2:
+            ListarContas(); 
+            break;
+        case 3:
+            Deposito();
+            break;
+
+        case 4:
+            Saque();
+            break;
+        case 5:
+            FiltrarSaldo();
+            break;
+        case 6:
+            SaldoTotal();
+            break;
+
+        default:
+            Console.WriteLine("Opção Inválida");
+            Console.WriteLine("\nPressione qualquer tecla para continuar...");
+            Console.ReadKey();
+            break;
+    }
+}
+
+void CadastrarConta()
+{
+    string opcContinuar = "s";
+
+    while (opcContinuar != "n")
+    {
+        ContaBancaria c1 = new ContaBancaria();
+        Console.Clear();
+        Console.WriteLine("Informe o ID da conta: ");
+        c1.IdConta = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Informe a agência da conta: ");
+        c1.Agencia = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Informe o número da conta: ");
+        c1.NuConta = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Informe o nome do titular:");
+        c1.TitularConta = Console.ReadLine();
+
+        Console.WriteLine("Informe o saldo da conta: ");
+        c1.Saldo = Convert.ToDouble(Console.ReadLine());
+
+        contas.Add(c1);
+
+        Console.Write("Deseja cadastrar uma nova conta (s/n):");
+        opcContinuar = Console.ReadLine().ToLower().Trim();
+
+    }
+}
+
+void ListarContas()
+{
+    Console.Clear();
+    if (contas.Count == 0)
+    {
+        Console.WriteLine("Não há contas cadastradas.");
+        Console.WriteLine("\nPressione qualquer tecla para continuar...");
+        Console.ReadKey();
+    }
+    else
+    {
+        Console.Clear();
+        foreach (ContaBancaria c in contas)
+        {
+            Console.WriteLine($"\nID da conta: {c.IdConta}");
+            Console.WriteLine($"Agência: {c.Agencia}");
+            Console.WriteLine($"Número da conta: {c.NuConta}");
+            Console.WriteLine($"Nome do titular: {c.TitularConta}");
+            Console.WriteLine($"Saldo da conta: {c.Saldo.ToString("C2")}");
+        }
+        Console.WriteLine("\nPressione qualquer tecla para continuar...");
+        Console.ReadKey();
+    }
+}
+
+void Deposito()
+{
+    Console.Clear();
+    Console.Write("Informe o ID da conta para depositar: ");
+    int idConta = Convert.ToInt32(Console.ReadLine());
+
+    ContaBancaria contaCli = contas.FirstOrDefault(x => x.IdConta == idConta);
+    //localizando a conta bancário correta antes de efetuar o depósito
+
+    if (contaCli != null)
+    {
+        Console.WriteLine($"\nTitular da Conta: {contaCli.TitularConta}");
+        Console.Write("\nInforme o valor do depósito: R$");
+        double valorDep = Convert.ToDouble(Console.ReadLine());
+        contaCli.Depositar(valorDep);
+        Console.WriteLine("\nDepósito realizado com sucesso!");
+    }
+    else
+    {
+        Console.WriteLine("Conta bancária não localizada.");
+    }
+    Console.WriteLine("\nPressione qualquer tecla para continuar...");
+    Console.ReadKey();
+}
+
+void Saque()
+{
+    Console.Clear();
+    Console.Write("Informe o ID da conta para sacar: ");
+    int idConta = Convert.ToInt32(Console.ReadLine());
+
+    ContaBancaria contaCli = contas.FirstOrDefault(x => x.IdConta == idConta);
+    //localizando a conta bancário correta antes de efetuar o saque
+
+    if (contaCli != null)
+    {
+        Console.WriteLine($"\nTitular da Conta: {contaCli.TitularConta}");
+        Console.Write("\nInforme o valor do saque: R$");
+        double valorSaque = Convert.ToDouble(Console.ReadLine());
+        contaCli.Sacar(valorSaque);
+        Console.WriteLine("\nSaque realizado com sucesso!");
+    }
+
+    Console.WriteLine("\nPressione qualquer tecla para continuar...");
+    Console.ReadKey();
+}
+
+void FiltrarSaldo()
+{
+    Console.Clear();
+    if (contas.Count == 0)
+    {
+        Console.WriteLine("Nenhuma conta cadastrada.");
+        Console.WriteLine("\nPressione qualquer tecla para continuar...");
+        Console.ReadKey();
+    }
+    else
+    {
+        string opcao = "s";
+        while (opcao != "n")
+        {
+            Console.Clear();
+            Console.Write("Informe o valor que deseja filtrar: R$");
+            double valorFiltro = Convert.ToDouble(Console.ReadLine());
+
+            List<ContaBancaria> filtroConta = contas.Where(x => x.Saldo >= valorFiltro).ToList();
+
+            if (filtroConta.Count == 0)
+            {
+                Console.WriteLine("Nenhuma conta corresponde ao filtro");
+                Console.WriteLine("\nDeseja fazer uma nova consulta (s/n): ");
+                opcao = Console.ReadLine().ToLower().Trim();
+            }
+            else
+            {
+                foreach (ContaBancaria c in filtroConta)
+                {
+                    Console.WriteLine($"\nID da conta: {c.IdConta}");
+                    Console.WriteLine($"Titular da conta: {c.TitularConta}");
+                    Console.WriteLine($"Saldo da conta: {c.Saldo.ToString("C2")}");
+                }
+
+                Console.WriteLine("\nDeseja fazer uma nova consulta (s/n): ");
+                opcao = Console.ReadLine().ToLower().Trim();
+            }
+        }
+        Console.WriteLine("Pressione qualquer tecla para continuar...");
+        Console.ReadKey();
+
+    }
+}
+void SaldoTotal()
+{
+    Console.Clear();
+    if (contas.Count == 0)
+    {
+        Console.WriteLine("Nenhuma conta cadastrada.");
+        Console.WriteLine("\nPressione qualquer tecla para continuar...");
+        Console.ReadKey();
+    }
+    else
+    {
+        double saldoTotal = contas.Sum(x => x.Saldo);
+        Console.WriteLine($"\nO saldo total das contas: {saldoTotal.ToString("C2")}");
+    }
+    Console.WriteLine("\nPressione qualquer tecla para continuar...");
+    Console.ReadKey();
+}
